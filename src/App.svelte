76effc7ba266355161005b504c3d88d13873d7f1
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-09 20:15:33 -0500

    Use the new components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
 <script>
-	export let name;
+  import Header from "./components/Header.svelte";
+  import RegistrationForm from "./components/RegistrationForm.svelte";
+  import UserList from "./components/UserList.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="lg:overflow-hidden h-screen">
+  <Header />
+  <div class="grid lg:grid-cols-2 w-full h-full">
+    <div class="bg-black lg:min-h-full">
+      <RegistrationForm />
+    </div>
+    <div class="lg:min-h-full">
+      <UserList />
+    </div>
+  </div>
+</div>
