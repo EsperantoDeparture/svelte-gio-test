af419a9ee2a1405cfec7028b7af1535059d98d03
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-09 20:15:13 -0500

    Registration form

---
+++ src/components/RegistrationForm.svelte
@@ -0,0 +1,20 @@
+<script>
+  import FileUpload from "./FileUpload.svelte";
+  import Input from "./Input.svelte";
+</script>
+
+<section class="px-2 lg:px-4 m-auto max-w-md py-8">
+  <h3
+    class="font-bold text-4xl tracking-widest text-center spaced-text text-white"
+  >
+    ENTER THE DATA TO THE REGISTER
+  </h3>
+  <form class="mx-12 mt-14 grid-cols-1 grid gap-6">
+    <Input placeholder="User Name" />
+    <Input placeholder="Email" />
+    <FileUpload />
+  </form>
+</section>
+
+<style>
+</style>
