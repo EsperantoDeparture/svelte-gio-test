720a094e1d7ec41707fc29bb9a4cf08ce7a045df
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-09 19:30:44 -0500

    Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<div class="p-4 bg-white flex justify-center shadow-md sticky top-0">
+  <img
+    src="https://getitout.io/wp-content/uploads/2020/04/GET-it-OUT.svg"
+    alt=""
+    sizes="(min-width: 0px) and (max-width: 0px) 0px, (min-width: 1px) 225px, 100vw"
+    srcset="https://getitout.io/wp-content/uploads/2020/04/GET-it-OUT.svg 225w, https://getitout.io/wp-content/uploads/2020/04/GET-it-OUT.svg 0w"
+  />
+</div>
