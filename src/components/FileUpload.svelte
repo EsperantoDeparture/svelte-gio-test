55aed4787ddbd64740430fe3af450720595f9cb3
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-09 20:14:51 -0500

    File upload box

---
+++ src/components/FileUpload.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let file;
+</script>
+
+<div
+  class="grid justify-center place-content-center place-items-center border border-white w-full h-48 cursor-pointer"
+>
+  <svg
+    width="42"
+    height="42"
+    viewBox="0 0 42 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M42 18H24V0H18V18H0V24H18V42H24V24H42V18Z" fill="white" />
+  </svg>
+  <p class="text-white mt-2">Upload image</p>
+</div>
