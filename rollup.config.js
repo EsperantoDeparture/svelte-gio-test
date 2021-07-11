c317d976cfdbdb97dc205b4edd26e63ac1742da3
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-10 22:39:56 -0500

    Setup typescript

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import sveltePreprocess from "svelte-preprocess";
 import postcss from "rollup-plugin-postcss";
+import typescript from "@rollup/plugin-typescript";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -35,7 +36,7 @@ function serve() {
 }
 
 export default {
-  input: "src/main.js",
+  input: "src/main.ts",
   output: {
     sourcemap: true,
     format: "iife",
@@ -53,6 +54,7 @@ export default {
           plugins: [require("tailwindcss"), require("autoprefixer")],
         },
       }),
+      preprocess: sveltePreprocess({ sourceMap: !production }),
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production,
@@ -72,6 +74,10 @@ export default {
       dedupe: ["svelte"],
     }),
     commonjs(),
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production,
+    }),
 
     // In dev mode, call `npm run start` once
     // the bundle has been generated
