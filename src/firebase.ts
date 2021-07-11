1445c45a435514b94ab301c70f39f23223a7f2f1
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-10 23:57:51 -0500

    Use .ts extension

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCxaqEMa-r7HbGZOGm4459lIoAerwhCWgk",
@@ -11,8 +12,10 @@ const firebaseConfig = {
   appId: "1:911709519516:web:a69d6dd01104193ed6b759",
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
+export const auth = firebase.auth(app);
 
-export const db = firebase.firestore();
+export const storage = firebase.storage(app);
+
+export const db = firebase.firestore(app);
