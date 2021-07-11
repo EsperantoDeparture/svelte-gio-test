86b615a4e3fb28364932365a2d3965b040ed0510
Juan Pablo Rodriguez Macias <juanpablordmsx2@gmail.com>
2021-07-11 15:25:48 -0500

    Prevent text overflow

--- src/components/User.svelte
+++ src/components/User.svelte
@@ -7,7 +7,11 @@
 <div class="rounded-lg shadow-lg max-w-60">
   <img class="w-full h-36 object-cover rounded-t-lg" alt="" src={photo} />
   <div class="p-4">
-    <h4 class="text-2xl font-medium text-center">{username}</h4>
-    <p class="text-lg text-center">{email}</p>
+    <h4
+      class="text-2xl overflow-ellipsis overflow-hidden font-medium text-center"
+    >
+      {username}
+    </h4>
+    <p class="text-lg overflow-ellipsis overflow-hidden text-center">{email}</p>
   </div>
 </div>
